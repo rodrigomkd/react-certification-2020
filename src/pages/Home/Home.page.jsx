9d4a6365581bb1d12d29e5214dd744c38d03a28c
rodrigomkd <rodrigo.moncada@live.com>
2021-04-21 19:28:47 -0500

    [ADDED] removed HomePage authentication validation

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,42 +1,15 @@
 import React, { useRef } from 'react';
-import { Link, useHistory } from 'react-router-dom';
 
-import { useAuth } from '../../providers/Auth';
 import './Home.styles.css';
-//import mockedData from '../../mock/youtube-videos-mock.json';
 import VideoList from '../../components/VideoList';
 
 function HomePage() {
-  const history = useHistory();
   const sectionRef = useRef(null);
-  const { authenticated, logout } = useAuth();
-
-  function deAuthenticate(event) {
-    event.preventDefault();
-    logout();
-    history.push('/');
-  }
 
   return (
     <div ref={sectionRef}>
-      <h1 className="title">Welcome to the Challenge!</h1>
-      {authenticated ? (
-        
-        <>
-        <VideoList />
-
-          <h2>Good to have you back</h2>
-          <span>
-            <Link to="/" onClick={deAuthenticate}>
-              ← logout
-            </Link>
-            <span className="separator" />
-            <Link to="/secret">show me something cool →</Link>
-          </span>
-        </>
-      ) : (
-        <Link to="/login">let me in →</Link>
-      )}
+      <h2 className="title">Welcome to the Challenge!</h2>
+      <VideoList />
     </div>
   );
 }
