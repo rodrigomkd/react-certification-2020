cfe67e9cf2d0e0d4ef313b0400a298d69be48eee
rodrigomkd <rodrigo.moncada@live.com>
2021-05-04 18:02:26 -0500

    Add files via upload

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,9 @@
 const AUTH_STORAGE_KEY = 'wa_cert_authenticated';
+const YOUTUBE_BASE_URL = "https://www.googleapis.com/youtube/v3";
+const YOUTUBE_EMBEDD_URL = "https://www.youtube.com/embed/";
 
-export { AUTH_STORAGE_KEY };
+export {
+  AUTH_STORAGE_KEY,
+  YOUTUBE_BASE_URL,
+  YOUTUBE_EMBEDD_URL
+};
